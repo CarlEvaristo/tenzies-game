e4e101782b811d984da322fa3b49f6e2b6aa4fb0
CarlEverest78 <thepythoneer@gmail.com>
2022-08-26 00:36:54 +0200

    debugged typography

--- client/src/Components/Highscore.js
+++ client/src/Components/Highscore.js
@@ -4,7 +4,7 @@ export default function Highscore(props){
     let highScoreElements
     if ( props.highScore.length !== undefined) {
         highScoreElements = (props.highScore).map((score,index) => {
-             return <li key={index}>{score.name} {score.score}</li>
+             return <li key={index}>{score.name} : {score.score} rolls</li>
         })
     } else {
         highScoreElements = "No Scores Yet"
