e775c1baf964bf5fa60b0132b2b0153a9b91bd73
CarlEverest78 <thepythoneer@gmail.com>
2022-08-26 12:45:33 +0200

    add dice images

--- client/src/Components/Dice.js
+++ client/src/Components/Dice.js
@@ -22,7 +22,8 @@ export default function Dice(props){
             style={{backgroundColor: props.isFinished ? "#59E391" : "#fff"}} 
             onClick={()=>props.handleClick(props.id, props.value)}
             >
-            <i class={diceValueChar}></i>
+            <p>{props.value}</p>
+            {/* <i class={diceValueChar}></i> */}
         </div>
     )
 }
