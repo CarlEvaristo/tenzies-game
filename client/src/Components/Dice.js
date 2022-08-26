901a6f015b6b9161ba07db4481a41e5c5c120f13
CarlEverest78 <thepythoneer@gmail.com>
2022-08-26 13:18:09 +0200

    add dice images

--- client/src/Components/Dice.js
+++ client/src/Components/Dice.js
@@ -1,6 +1,20 @@
 import React from "react"
 
 export default function Dice(props){
+    let diceValueChar = ""
+    if (props.value === 1) {
+        diceValueChar = "fa-solid fa-dice-one"
+    } else if (props.value === 2) {
+        diceValueChar = "fa-solid fa-dice-two"
+    } else if (props.value === 3) {
+        diceValueChar = "fa-solid fa-dice-three"
+    } else if (props.value === 4) {
+        diceValueChar = "fa-solid fa-dice-four"
+    } else if (props.value === 5) {
+        diceValueChar = "fa-solid fa-dice-five"
+    } else {
+        diceValueChar = "fa-solid fa-dice-six"
+    }
 
     return(
         <div 
@@ -8,7 +22,8 @@ export default function Dice(props){
             style={{backgroundColor: props.isFinished ? "#59E391" : "#fff"}} 
             onClick={()=>props.handleClick(props.id, props.value)}
             >
-                <p>{props.value}</p>
+            {/* <p>{props.value}</p> */}
+            <i class={diceValueChar}></i>
         </div>
     )
 }
