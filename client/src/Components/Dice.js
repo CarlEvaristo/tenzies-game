a24470192e70116dd961331745e3e8bdd2ca2f0f
CarlEverest78 <thepythoneer@gmail.com>
2022-08-26 12:51:46 +0200

    add dice images

--- client/src/Components/Dice.js
+++ client/src/Components/Dice.js
@@ -1,20 +1,20 @@
 import React from "react"
 
 export default function Dice(props){
-    let diceValueChar = ""
-    if (props.value === 1) {
-        diceValueChar = "fa-solid fa-dice-one"
-    } else if (props.value === 2) {
-        diceValueChar = "fa-solid fa-dice-two"
-    } else if (props.value === 3) {
-        diceValueChar = "fa-solid fa-dice-three"
-    } else if (props.value === 4) {
-        diceValueChar = "fa-solid fa-dice-four"
-    } else if (props.value === 5) {
-        diceValueChar = "fa-solid fa-dice-five"
-    } else {
-        diceValueChar = "fa-solid fa-dice-six"
-    }
+    // let diceValueChar = ""
+    // if (props.value === 1) {
+    //     diceValueChar = "fa-solid fa-dice-one"
+    // } else if (props.value === 2) {
+    //     diceValueChar = "fa-solid fa-dice-two"
+    // } else if (props.value === 3) {
+    //     diceValueChar = "fa-solid fa-dice-three"
+    // } else if (props.value === 4) {
+    //     diceValueChar = "fa-solid fa-dice-four"
+    // } else if (props.value === 5) {
+    //     diceValueChar = "fa-solid fa-dice-five"
+    // } else {
+    //     diceValueChar = "fa-solid fa-dice-six"
+    // }
 
     return(
         <div 
